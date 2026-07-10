1402f672fe99b567a499dc5191e9a8d25e237b7c
Berke-inan <i.berkeinan@gmail.com>
2026-07-11 01:06:21 +0300

    Character Movement Added

---
+++ Assets/Scripts/Player/CameraManager.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using Unity.Cinemachine; // Cinemachine 3 için gerekli kütüphane
+
+namespace FarmerSimulator.Player
+{
+    [RequireComponent(typeof(PlayerInputHandler))]
+    public class CameraManager : MonoBehaviour
+    {
+        [Header("Cinemachine Kameraları")]
+        [SerializeField] private CinemachineCamera fpsCamera;
+        [SerializeField] private CinemachineCamera tpsCamera;
+
+        private PlayerInputHandler _inputHandler;
+        private bool _isFPS = true;
+
+        private void Awake()
+        {
+            _inputHandler = GetComponent<PlayerInputHandler>();
+        }
+
+        private void OnEnable()
+        {
+            // Olayı dinlemeye başla
+            _inputHandler.OnSwitchCamera += ToggleCamera;
+        }
+
+        private void OnDisable()
+        {
+            // Dinlemeyi bırak
+            _inputHandler.OnSwitchCamera -= ToggleCamera;
+        }
+
+        private void ToggleCamera()
+        {
+            _isFPS = !_isFPS;
+
+            // Önceliği yüksek olan kamera aktif olur
+            if (_isFPS)
+            {
+                fpsCamera.Priority = 12;
+                tpsCamera.Priority = 10;
+            }
+            else
+            {
+                tpsCamera.Priority = 12;
+                fpsCamera.Priority = 10;
+            }
+        }
+    }
+}
